275b99656705e1e5a69a90ed658c130f94107c2f
Felipe Pontes <felipemfpontes@gmail.com>
2017-06-25 14:19:29 -0300

    Fix people migrations (#22)

--- backend/migrations/4_create_federative_units_table.sql
+++ backend/migrations/4_create_federative_units_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS federative_units (
   id SERIAL PRIMARY KEY,  
   country_id INTEGER NOT NULL REFERENCES countries(id),
   name VARCHAR(255) NOT NULL,
-  abbr VARCHAR(3) NOT NULL,
+  abbr VARCHAR(3) NOT NULL
 );
--- backend/migrations/5_create_cities_table.sql
+++ backend/migrations/5_create_cities_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS cities (
   id SERIAL PRIMARY KEY,  
   federative_unit_id INTEGER NOT NULL REFERENCES federative_units(id),
-  name VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
 );
