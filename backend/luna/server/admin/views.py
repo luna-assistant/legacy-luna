23a9fa1cb95b337d36501c5c49cc6d45ecd89007
Felipe Pontes <felipemfpontes@gmail.com>
2017-07-06 13:12:41 -0300

    Add admin required to admin blueprint

--- backend/luna/server/admin/views.py
+++ backend/luna/server/admin/views.py
@@ -1,5 +1,12 @@
-from flask import Blueprint, json
+from flask import Blueprint, json, current_app
 from flask.views import MethodView
+from flask_login import current_user
+from luna.server.models import Role
 
 
 admin_blueprint = Blueprint('admin', __name__)
+
+@admin_blueprint.before_request
+def admin_required():
+    if not current_user.is_authenticated or not current_user.has_role(Role.ADMIN):
+        return current_app.login_manager.unauthorized()
