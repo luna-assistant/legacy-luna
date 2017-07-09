bcdd6a62afc9299e17b06e32c3ab3ade0a896517
yuriscosta <yuri.sales@protonmail.com>
2017-07-09 13:15:09 -0300

    altera o modal de redefinir a senha, inserindo um campo pra cpf, senha e confirmação #34

--- backend/luna/server/auth/forms.py
+++ backend/luna/server/auth/forms.py
@@ -5,7 +5,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from luna.server.validators import CPF
 from luna.server.repositories import UserRepository
+from luna.server.repositories import PersonRepository
+from brazilnum.util import clean_id
+from flask_login import current_user
 
 def unique_username(message='O campo informado já existe'):
     def _unique_username(form, field):
@@ -14,6 +18,13 @@ def unique_username(message='O campo informado já existe'):
             raise ValidationError(message)
 
     return _unique_username
+
+def unique_cpf(message='O campo informado já existe'):
+    def _unique_cpf(form, field):
+        person = PersonRepository().findByCpf(clean_id(field.data))
+        if person and person.user_id != current_user.id:
+            raise ValidationError(message)
+    return _unique_cpf
 
 class LoginForm(FlaskForm):
     email = EmailField(
@@ -26,11 +37,22 @@ class LoginForm(FlaskForm):
 
 
 class NewPasswordForm(FlaskForm):
-    email = EmailField(
-        'Email do Usuário',
+    cpf = StringField('CPF', [
+        DataRequired(),
+        Length(max=14),
+        CPF(),
+        unique_cpf()
+    ])
+    password = PasswordField(
+        'Nova senha',
+       validators=[DataRequired(message='O campo Senha é obrigatório'), Length(min=6, max=25)]
+    )
+    confirm = PasswordField(
+        'Confirmação',
         validators=[
-            DataRequired(message = 'O campo Email é obrigatório'),
-            Length(min=10)
+            DataRequired(message='O campo de confirmação da senha é obrigatório'),
+            EqualTo(
+                'password', message='A confirmação deve ser igual à senha informada.')
         ]
     )
 
